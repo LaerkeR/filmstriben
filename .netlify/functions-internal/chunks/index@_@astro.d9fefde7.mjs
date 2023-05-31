532b534406cea8ad7c06fb5b9886c032dd13f483
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2023-05-31 14:51:20 +0200

    try againg

---
+++ .netlify/functions-internal/chunks/index@_@astro.d9fefde7.mjs
@@ -0,0 +1,13 @@
+export { renderers } from '../renderers.mjs';
+import './astro.9b869f1a.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import 'slash';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/index.astro.ab47f182.mjs');
+
+export { page };
