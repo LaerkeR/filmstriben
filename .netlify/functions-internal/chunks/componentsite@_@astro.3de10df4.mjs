1f58df5063cabe2431fda863d472f35fe93e3809
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2023-05-31 14:43:08 +0200

    lots of things

---
+++ .netlify/functions-internal/chunks/componentsite@_@astro.3de10df4.mjs
@@ -0,0 +1,13 @@
+export { renderers } from '../renderers.mjs';
+import './astro.9b869f1a.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import 'slash';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/componentsite.astro.bf48b573.mjs').then(n => n.d);
+
+export { page };
