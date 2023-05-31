0a589cdc9a48f5770be272a0c84ac464acfbe21d
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2023-05-31 15:00:04 +0200

    igen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: netlify()
 });
